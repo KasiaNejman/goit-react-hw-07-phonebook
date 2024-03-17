fb61bf8cc9ea1269e23c3f3cb5ce3c82bb686967
KasiaNejman <139686065+KasiaNejman@users.noreply.github.com>
2024-03-17 16:43:30 +0100

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,22 +1,22 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-
-axios.defaults.baseURL = "https://65d4f01b3f1ab8c634364efe.mockapi.io/contacts";
+axios.defaults.baseURL =
+  'https://65f70d1cb4f842e808850706.mockapi.io/contacts/';
 
 const initialState = {
   contacts: [],
 };
 
-export const fetchTasks = createAsyncThunk("tasks/fetchAll", async () => {
-  const response = await axios.get("/contacts");
+export const fetchTasks = createAsyncThunk('tasks/fetchAll', async () => {
+  const response = await axios.get('/contacts');
   return response.data;
 });
 
 // Asynchroniczny thunk do dodawania nowego kontaktu
 export const addContactAsync = createAsyncThunk(
   'contacts/addContact',
-  async (contactData) => {
+  async contactData => {
     const response = await axios.post('/contacts', contactData); // '/contacts' to endpoint API
     return response.data;
   }
@@ -33,37 +33,33 @@ export const fetchContactsAsync = createAsyncThunk(
 // Asynchroniczny thunk do usuwania kontaktu
 export const deleteContactAsync = createAsyncThunk(
   'contacts/deleteContact',
-  async (contactId) => {
+  async contactId => {
     await axios.delete(`/contacts/${contactId}`);
     return contactId;
   }
 );
 
 export const contactsSlice = createSlice({
-  name: "contacts",
+  name: 'contacts',
   initialState,
-  reducers: {
-  },
-  extraReducers: (builder) => {
+  reducers: {},
+  extraReducers: builder => {
     builder
-      .addCase(addContactAsync.pending, (state) => {
-        
-      })
+      .addCase(addContactAsync.pending, state => {})
       .addCase(addContactAsync.fulfilled, (state, action) => {
-        state.contacts.push(action.payload); 
+        state.contacts.push(action.payload);
       })
-      .addCase(addContactAsync.rejected, (state) => {
-      })
+      .addCase(addContactAsync.rejected, state => {})
       .addCase(fetchContactsAsync.fulfilled, (state, action) => {
-        state.contacts = action.payload; 
+        state.contacts = action.payload;
       })
       .addCase(deleteContactAsync.fulfilled, (state, action) => {
-        state.contacts = state.contacts.filter(contact => contact.id !== action.payload); 
+        state.contacts = state.contacts.filter(
+          contact => contact.id !== action.payload
+        );
       });
-    
   },
 });
 
 export const { addContact, deleteContacts } = contactsSlice.actions;
 export const contactReducer = contactsSlice.reducer;
-
