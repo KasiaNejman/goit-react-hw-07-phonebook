a6e05fa1fb1291d43059737a7132fc0448a0a846
KasiaNejman <139686065+KasiaNejman@users.noreply.github.com>
2024-03-17 15:30:23 +0100

    Create ContactList.jsx

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,33 @@
+import PropTypes from "prop-types";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteContactAsync } from "../../redux/contactsSlice";
+const ContactList = () => {
+  const contacts = useSelector((state) => state.contacts.contacts);
+  const filter = useSelector((state) => state.filter);
+  const dispatch = useDispatch();
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <ul>
+      {filteredContacts.map((contact) => {
+        return (
+          <li key={contact.id}>
+            {contact.name}: {contact.number}
+            <button onClick={() => dispatch(deleteContactAsync(contact.id))}>
+              Delete
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default ContactList;
+
+ContactList.propTypes = {
+  contacts: PropTypes.array,
+};
