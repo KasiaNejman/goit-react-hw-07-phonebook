3fdbf8a92ba47216019d1111916e0df4795294ee
KasiaNejman <139686065+KasiaNejman@users.noreply.github.com>
2024-03-17 15:29:43 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux"; 
+import { fetchContactsAsync } from "../src/redux/contactsSlice"; 
+import Section from "./components/Section/Section";
+import ContactForm from "./components/ContactForm/ContactForm";
+import Contacts from "./components/ContactList/ContactList";
+import Filter from "./components/Filter/Filter";
+import "./App.css";
+
+const App = () => {
+  const dispatch = useDispatch(); 
+
+  useEffect(() => {
+    dispatch(fetchContactsAsync());
+  }, [dispatch]); 
+
+  return (
+    <>
+      <Section title="Phonebook">
+        <ContactForm />
+      </Section>
+      <Section title="Contacts">
+        <Filter />
+        <Contacts />
+      </Section>
+    </>
+  );
+};
+
+export default App;
+
