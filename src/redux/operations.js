dc55890fe6cb706b219ed87b62d2ce65acaec824
KasiaNejman <139686065+KasiaNejman@users.noreply.github.com>
2024-06-09 13:14:53 +0200

    Create operations.js

---
+++ src/redux/operations.js
@@ -0,0 +1,51 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://65e049fcd3db23f76248d959.mockapi.io';
+
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetch',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('/contacts/contacts');
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const addContact = createAsyncThunk(
+  'contacts/add',
+  async (contact, thunkAPI) => {
+    try {
+      const existingContacts = thunkAPI.getState().contacts;
+      const isDuplicate = existingContacts.some(
+        c => c.name.toLowerCase() === contact.name.toLowerCase()
+      );
+
+      if (isDuplicate) {
+        return thunkAPI.rejectWithValue(
+          'Contact with the same name already exists.'
+        );
+      }
+
+      const response = await axios.post('/contacts/contacts', contact);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  'contacts/delete',
+  async (contactId, thunkAPI) => {
+    try {
+      await axios.delete(`/contacts/contacts/${contactId}`);
+      return contactId;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
